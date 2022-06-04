c7ce8564571a45ba74977fc599ed40aec81ef480
Akash Kumar Verma <akash@yallafelsekka.com>
2022-06-05 02:04:34 +0530

    Import Export

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { read, utils, writeFile } from 'xlsx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-excel-csv';
+    movies: any[] = [];
+
+    handleImport($event: any) {
+        const files = $event.target.files;
+        if (files.length) {
+            const file = files[0];
+            const reader = new FileReader();
+            reader.onload = (event: any) => {
+                const wb = read(event.target.result);
+                const sheets = wb.SheetNames;
+
+                if (sheets.length) {
+                    const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+                    this.movies = rows;
+                }
+            }
+            reader.readAsArrayBuffer(file);
+        }
+    }
+
+    handleExport() {
+        const headings = [[
+            'Movie',
+            'Category',
+            'Director',
+            'Rating'
+        ]];
+        const wb = utils.book_new();
+        const ws: any = utils.json_to_sheet([]);
+        utils.sheet_add_aoa(ws, headings);
+        utils.sheet_add_json(ws, this.movies, { origin: 'A2', skipHeader: true });
+        utils.book_append_sheet(wb, ws, 'Report');
+        writeFile(wb, 'Movie Report.xlsx');
+    }
 }
